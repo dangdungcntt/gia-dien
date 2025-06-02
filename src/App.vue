7c7a049ab2983878ecbdf22f968989e5b997ae82
Dung Nguyen Dang <dangdungcntt@gmail.com>
2025-06-02 12:37:33 +0700

    chore: update reference

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ const state = reactive({
 
 <template>
   <div class="container mx-auto p-4 space-y-4">
-    <h1 class="text-center my-4 text-3xl font-medium">Tính tiền điện theo quyết định số 14/2025/QĐ-TTg của Thủ tướng Chính phủ</h1>
+    <h1 class="text-center my-4 text-3xl font-medium">Tính tiền điện theo biểu giá mới từ 29/05/2025 <a class="text-sm text-blue-500" href="https://infographics.vn/gia-ban-le-dien-sinh-hoat-giam-con-5-bac/215826.vna" target="_blank">[nguồn]</a></h1>
     <div class="grid md:grid-cols-2 gap-4 md:gap-8 mb-4">
       <div>
         <label>Nhập số kWh</label>
